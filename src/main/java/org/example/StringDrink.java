7f9e301900a62e0a66f1323ce009912eece00035
José Luís Barbosa Soares da Silva <up201906576@up.pt>
2022-11-14 16:24:14 +0000

    Added String Drink test and class

---
+++ src/main/java/org/example/StringDrink.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class StringDrink {
+
+    private String text;
+
+    public StringDrink(String text){
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text= text;
+    }
+}
